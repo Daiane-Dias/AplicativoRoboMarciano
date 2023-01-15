cff936a5ba30ddfaef05c565ae17cdcdb5be1f9d
Daiane Dias <diasdaianedd@gmail.com>
2023-01-15 18:55:01 -0300

    first commit

---
+++ app/src/main/java/com/daiane/aplicativorobo/MainActivity2.kt
@@ -0,0 +1,35 @@
+package com.daiane.aplicativorobo
+
+import MarcianoAvancado
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.TextInputLayout
+import android.view.View
+import android.widget.Button
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+        var mensagem = findViewById<TextInputLayout>(R.id.textoEnviado)
+        var botaoEnviar = findViewById<Button>(R.id.button_first)
+        botaoEnviar.setOnClickListener(
+            View.OnClickListener {
+                exibeMensagem(mensagem.editText.toString())
+            })
+
+
+
+    }
+    fun exibeMensagem(mensagem: String) {
+        var intent = Intent(this,RespostaActivity::class.java)
+        var marciano = MarcianoAvancado()
+        intent.putExtra("resposta", marciano.responda(mensagem))
+        startActivity(intent)
+
+    }
+
+}
